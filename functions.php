74da54c0f1d58b37057e2d7dcafcef033d24be55
Kevin Bollman <digitalkdogg@gmail.com>
2022-03-12 08:53:52 -0600

    increase height on form payment for donate page

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ if ( ! function_exists( 'enqueue_custom_stuff' ) ) {
 
     if (is_page_template( 'page-templates/donate-page.php' )) {
             wp_enqueue_script( 'donatejs', get_stylesheet_directory_uri() . '/assets/js/min/donate.min.js' );
-            wp_enqueue_style( 'donatecss', get_stylesheet_directory_uri() . '/assets/css/donate.css' );
+            wp_enqueue_style('donatecss', get_stylesheet_directory_uri() . '/assets/css/donate.css', array(), '1.0.1', false);
     }
     if (is_page_template( 'page-templates/news-page.php' )) {
             wp_enqueue_script( 'donatejs', get_stylesheet_directory_uri() . '/assets/js/min/news.min.js' );
