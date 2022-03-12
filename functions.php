6a829d3f6fcdcd83e85c1b682a3861e0af178d2d
Kevin Bollman <digitalkdogg@gmail.com>
2022-03-12 09:41:00 -0600

    maring none issue

--- functions.php
+++ functions.php
@@ -25,7 +25,7 @@ if ( ! function_exists( 'enqueue_custom_stuff' ) ) {
 	function enqueue_custom_stuff() {
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', 'before');
     wp_enqueue_script( 'customjs', get_stylesheet_directory_uri() . '/assets/js/min/base.min.js' , array(), '1.0.1', false);
-    wp_enqueue_style( 'millwood-base', get_stylesheet_directory_uri() . '/assets/css/base.css', array(), '1.0.2', false);
+    wp_enqueue_style( 'millwood-base', get_stylesheet_directory_uri() . '/assets/css/base.css', array(), '1.0.3', false);
 
     if (is_page_template( 'page-templates/donate-page.php' )) {
             wp_enqueue_script( 'donatejs', get_stylesheet_directory_uri() . '/assets/js/min/donate.min.js', array(), '1.0.1', false );
